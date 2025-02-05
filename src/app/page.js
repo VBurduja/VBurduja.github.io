15ad2b981f511223a1a6e0c60b0cce58d396cb0b
VBurduja <volk3r513@gmail.com>
2025-02-05 14:26:20 +0200

    dutch-blitz-calc

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,97 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import Spreadsheet from "react-spreadsheet";
+import { useState, useEffect } from "react";
+import styles from "./page.module.css"
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [names, setNames] = useState(["Denis"]);
+  const [roundScore, setRoundScore] = useState([[0, 0]])
+  const [score, setScore] = useState([])
+  const [page, setPage] = useState(0)
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const zip = (...arr) => Array(Math.max(...arr.map(a => a.length))).fill().map((_, i) => arr.map(a => a[i]));
+
+  function onChangeName(e) {
+    setNames(e.map(row => row[0].value))
+  }
+  function onChangeRS(e) {
+    setRoundScore(e.map(row => [row[0].value, row[1].value]))
+  }
+
+  function onChangeScore(e) {
+    setScore(e.map(row => row.map(v => v?.value)))
+  }
+
+  function addPlayer() {
+    setNames([...names, ""])
+    setRoundScore([...roundScore, [0, 0]])
+  }
+  function addScore() {
+    setScore([...score, roundScore.map(rs => {
+      if (isNaN(rs[1]) || isNaN(rs[0]))
+        return 0
+      else
+        return Number(rs[1]) - Number(rs[0])
+    })])
+    setRoundScore(roundScore.map(rs => [0, 0]))
+    console.log(score)
+  }
+
+
+  return (
+    <>
+      <>
+        <div>
+          <button onClick={(e) => setPage(page - 1)}>PREV</button>
+          <button onClick={(e) => setPage(page + 1)}>NEXT</button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        {(page % 4 === 0) ?
+          <>
+            <Spreadsheet columnLabels={["Names"]} data={names.map(name => [{ value: name }])} onChange={onChangeName} />
+            <button onClick={addPlayer}>add player</button>
+          </> :
+          (page % 4 === 1) ?
+            <>
+              <Spreadsheet rowLabels={names} columnLabels={["Minus", "Plus"]} data={roundScore.map(score => [{ value: score[0] }, { value: score[1] }])} onChange={onChangeRS} />
+              <button onClick={addScore}>add score</button>
+            </> :
+            (page % 4 === 2) ?
+              <>
+                <Spreadsheet columnLabels={names} data={score.map(row => row?.map(v => { return { value: v } }))} onChange={onChangeScore} />
+              </> :
+              <>
+                {(score.length === 0) ? <Spreadsheet data={[[{ value: "" }]]} columnLabels={["Denis"]} /> :
+                  <Spreadsheet
+                    columnLabels={["Name", "Score"]}
+                    data={zip(
+                      names,
+                      zip(...score)
+                    ).map(
+                      row => [{ value: row[0] }, { value: row[1]?.reduce((a, b) => (isNaN(a) ? 0 : Number(a)) + (isNaN(b) ? 0 : Number(b)), 0) }]
+                    ).sort((a, b) => b[1].value - a[1].value)} />
+                }</>
+        }
+      </>
+
+
+      {/* <Spreadsheet columnLabels={["Names"]} data={names.map(name => [{ value: name }])} onChange={onChangeName} /> 
+      <button onClick={addPlayer}>add player</button>
+      <Spreadsheet rowLabels={names} columnLabels={["Minus", "Plus"]} data={roundScore.map(score => [{ value: score[0] }, { value: score[1] }])} onChange={onChangeRS} />
+      <button onClick={addScore}>add score</button>
+      <Spreadsheet columnLabels={names} data={score.map(row => row?.map(v => { return { value: v } }))} onChange={onChangeScore} />
+
+      {(score.length === 0) ? <Spreadsheet data={[[{ value: "" }]]} columnLabels={["Denis"]} /> :
+        <Spreadsheet
+          columnLabels={["Name", "Score"]}
+          data={zip(
+            names,
+            zip(...score)
+          ).map(
+            row => [{ value: row[0] }, { value: row[1]?.reduce((a, b) => (isNaN(a) ? 0 : Number(a)) + (isNaN(b) ? 0 : Number(b)), 0) }]
+          ).sort((a, b) => b[1].value - a[1].value)} />
+      } */}
+    </>
+  )
+
 }
+
